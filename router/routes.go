d4e56eb64af1959443b0fa0455b8444b5de8e8ac
Kaique <kaiquesantiagop@gmail.com>
2025-04-17 19:25:55 -0300

    feat: makefile and swagger implemented

--- router/routes.go
+++ router/routes.go
@@ -2,13 +2,18 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	docs "github.com/kSantiagoP/gopi/docs"
 	"github.com/kSantiagoP/gopi/handler"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initializeRoutes(router *gin.Engine) {
-
+	//initialize handler
 	handler.InitializeHandler()
-	v1 := router.Group("/api/v1")
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
+	v1 := router.Group(basePath)
 	{
 		v1.GET("/ping", handler.Ping)
 		v1.GET("/opening", handler.ShowOpeningHandler)
@@ -17,4 +22,7 @@ func initializeRoutes(router *gin.Engine) {
 		v1.PUT("/opening", handler.UpdateOpeningHandler)
 		v1.GET("/openings", handler.ListOpeningHandler)
 	}
+
+	//initialize swagger
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
