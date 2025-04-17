0b52d7799cbf571b051c2424fb0420f321c02e5a
Kaique <kaiquesantiagop@gmail.com>
2025-04-17 20:03:57 -0300

    feat: added documentation for listOpenings

--- handler/listOpening.go
+++ handler/listOpening.go
@@ -7,6 +7,16 @@ import (
 	"github.com/kSantiagoP/gopi/schemas"
 )
 
+// @BasePath /api/v1
+
+// @Summary List openings
+// @Description list all job openings
+// @Tags Openings
+// @Accept json
+// @Produce json
+// @Success 200 {object} ListOpeningsResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /openings [get]
 func ListOpeningHandler(c *gin.Context) {
 	openings := []schemas.Opening{}
 
